416e29a02a80469511f7d0a3185b101c452cdad0
SANGWOO PARK <cyberprophet@hanmail.net>
2022-10-30 01:33:42 +0900

    초기 커밋.

---
+++ Algorithmic.Windows/Program.cs
@@ -0,0 +1,12 @@
+namespace ShareInvest
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Securities());
+        }
+    }
+}
